5af864845f50239bf211c445030f3832ff14573a
Denis <56550245+Kaktusryak@users.noreply.github.com>
2020-06-03 10:36:46 +0300

    OOP7

---
+++ OOP7/OOP7/OOP7.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#include <conio.h>
+#include <iostream>
+
+struct element                  //структура даних
+{
+    short x;                        //значення елементу
+    element* Next;                 //адреса наступного елемементу
+
+};
+
+class List                      //Клас Список
+{
+    element* Head;                 //голова списку
+public:
+    List() { Head = NULL; }           //конструктор з ініціалізацією голови
+                           
+    void Add(short x);               //функція додавання значення в список
+    void Show();                   //функція відображення списку
+    void Count7Func();             //функція підрахунку кратних 7                                         
+    void ToZero();                  //функція переназначення як нуль значень, що більше середнього                
+private:
+    int count;
+};
+
+
+
+void List::Add(short x) //функція додавання значення в список
+{
+    element* temp = new element;           //виділення нової комірки
+    temp->x = x;                            //запис значення
+    temp->Next = Head;                      //наступний - голова
+    Head = temp;                            //останній - тільки що введений
+}
+
+
+
+void List::Show()                       //функція відображення списку
+{
+    element* temp = Head;                 //виділяємо покажчик на перший елемент
+
+    
+    while (temp != NULL)                   //до кінця
+    {
+        cout << temp->x << " ";               //виведення значення
+        temp = temp->Next;                    //перехід на наступний
+    }
+}
+void List::Count7Func()                       //функція підрахунку кратних 7    
+{
+    element* temp = Head;                 //виділяємо покажчик на перший елемент
+
+
+
+    while (temp != NULL)                    //до кінця
+    {
+        if ((temp->x % 7) == 0) {
+            count++;
+        }
+
+        temp = temp->Next;                    //перехід на наступний
+    }
+    cout << "Count = " << count << endl;
+}
+void List::ToZero()                       //функція переназначення як нуль значень, що більше середнього
+{
+    element* temp = Head;                 //виділяємо покажчик на перший елемент
+    short c = 0;                        //сума
+    short n = 0;                        //кількість
+
+    while (temp != NULL)                   //до кінця
+    {
+        c += temp->x;              
+        temp = temp->Next;                    //перехід на наступний
+        n++;
+    }
+    short t = c / n;                        //середнє
+    element* temp1 = Head;
+    while (temp1 != NULL)                  //до кінця
+    {
+        if (temp1->x > t) {                 //більше середнього
+            temp1->x = 0;
+       }
+        temp1 = temp1->Next;                //перехід на наступний
+    }
+   
+}
+
+
+int main()
+{
+    cout << "Denis Shulman IS-92 Var 15" << endl;
+    List L;//ініціалізація списку
+    L.Add(7);//дозапис елементу
+    L.Add(5);//дозапис елементу
+    L.Add(7);//дозапис елементу
+    L.Show();//виведення
+    cout << endl << "%7==0:" << endl;
+    L.Count7Func();//підрахунок кратних 7
+    cout << "After change :" << endl;
+    L.ToZero();//більше середнього = 0
+    L.Show();//виведення
+
+
+
+    cin.get();
+    return 0;
+}
---
+++ OOP7cs/OOP7cs/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7cs
+{
+    public class Node
+    {
+        private short _Data;//значення
+        private Node _Next;//наступний
+        
+        public short Value//властивість значення
+        {
+            get { return _Data; }
+            set { _Data = value; }
+        }
+        public Node(short Data)//конструктор запису даних
+        {
+            this._Data = Data;
+        }
+        public Node Next//наступний
+        {
+            get { return this._Next; }
+            set { this._Next = value; }
+        }
+
+    }
+
+
+    class List
+    {
+        private Node Head;//голова
+       
+        private int count;//кількість
+
+        public List()//конструктор ініціалізації голови
+        {
+           
+            Head  = null;
+        }
+        public void Add(short x)//додавання елементу
+        {
+
+            Node temp = new Node(x);//значення = х
+            temp.Next = Head;//накступний - голова
+            Head = temp;//останній той, що щойно введений
+
+        }
+        public void Show()//виведення елементу
+        {
+            Node temp = Head;//початок з голови
+            while (temp != null)//до кінця
+            {
+                Console.Write(temp.Value);
+                temp = temp.Next;//на наступний 
+            }
+            Console.WriteLine();
+        }
+        public void Count7Func()//функція підрахунку кратних 7 
+        {
+            Node temp = Head;//початок з голови
+            while (temp != null) //до кінця                  
+            {
+                if ((temp.Value % 7) == 0)//якщо кратне 7
+                {
+                    count++;
+                }
+
+                temp = temp.Next;//на наступний
+            }
+            Console.WriteLine("Count = {0}", count);
+            
+        }
+        public void ToZero()
+        {
+            Node temp = Head;//початок з голови
+            short c = 0;//сума
+            short n = 0;//кількість
+            while (temp != null)//до кінця
+            {
+                c += temp.Value;
+                n++;
+                temp = temp.Next;//на наступний
+            }
+            int t = c / n;
+            temp = Head;//знову початок з голови
+            while (temp != null)//до кінця
+            {
+              if(temp.Value > t)
+                {
+                    temp.Value = 0;
+                }
+                temp = temp.Next;//на наступний
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Denis Shulman IS-92 Var 15");
+            List L = new List();//створення списку
+            L.Add(7);//дозапис 
+            L.Add(5);//дозапис 
+            L.Add(7);//дозапис
+            L.Show();//виведення
+            Console.WriteLine("%7==0:");
+            L.Count7Func();//кратні 7
+            L.ToZero();//більші за середнє = 0
+            Console.WriteLine("After change:");
+            L.Show();//виведення
+            Console.ReadKey();
+        }
+    }
+}
+
